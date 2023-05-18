c456c98df85622360d1357f4c7694b50d67438bf
Polo <polo@Polos-MacBook-Pro.local>
2023-05-17 23:48:22 -0600

    added user creation to db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from '../../utilities/firebase/firebase.utilities';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utilities/firebase/firebase.utilities';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     };
 
     return (
--- src/utilities/firebase/firebase.utilities.js
+++ src/utilities/firebase/firebase.utilities.js
@@ -1,25 +1,60 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDdywwosxhPWbb41dUiOGbP9uRIw94odWU",
-    authDomain: "crwn-clothing-db-f1e8b.firebaseapp.com",
-    projectId: "crwn-clothing-db-f1e8b",
-    storageBucket: "crwn-clothing-db-f1e8b.appspot.com",
-    messagingSenderId: "749770346134",
-    appId: "1:749770346134:web:0a66025d32b4b9fe873cb7"
-  };
-  
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+  apiKey: 'AIzaSyDdywwosxhPWbb41dUiOGbP9uRIw94odWU',
+  authDomain: 'crwn-clothing-db-f1e8b.firebaseapp.com',
+  projectId: 'crwn-clothing-db-f1e8b',
+  storageBucket: 'crwn-clothing-db-f1e8b.appspot.com',
+  messagingSenderId: '749770346134',
+  appId: '1:749770346134:web:0a66025d32b4b9fe873cb7',
+};
 
-  // Configure auth provider
-  const provider = new GoogleAuthProvider();
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
 
-  provider.setCustomParameters({
-    prompt: 'select_account'   // Forces user to select account
-  });
+// Configure auth provider
+const provider = new GoogleAuthProvider();
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+provider.setCustomParameters({
+  prompt: 'select_account', // Forces user to select account
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+// Create the database
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (error) {
+            console.log( 'Error creating user', error.message );
+        }
+    }
+
+    return userDocRef;
+}
