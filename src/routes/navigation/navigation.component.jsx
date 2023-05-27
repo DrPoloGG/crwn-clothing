170ac1e2a6cefeb56a0dcd15011fb574361c6d4e
Polo <polo@Polos-MacBook-Pro.local>
2023-05-26 21:21:15 -0600

    weird conflict, pushing cart-context development straight to main

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,13 +5,17 @@ import { signOutUser } from '../../utilities/firebase/firebase.utilities';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
-import { UserContext } from '../../contexts/user/user.context';
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
-
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -24,16 +28,18 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          {
-            currentUser ? (
-              <span className='nav-link' onClick={ signOutUser }>SIGN OUT</span>
-            ) : (
-              <Link className='nav-link' to='/auth'>
-                SIGN IN
-              </Link>
-            )
-          }
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
         </div>
+        { isCartOpen && <CartDropdown /> }
       </div>
       <Outlet />
     </Fragment>
